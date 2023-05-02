c876e8d12b16ec59b8fd1b867a1703d534263e91
Anurag7275977 <arjunsharma705463@gmail.com>
2023-05-03 00:48:20 +0530

    fetch assignment Complete

---
+++ src/Component/Fetch.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+export default function DogImage() {
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => {
+        setImageUrl(data.message);
+      })
+      .catch((error) => {
+        console.error('Error fetching dog image:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <img src={imageUrl} alt="Random dog" />
+    </div>
+  );
+}
+
+
